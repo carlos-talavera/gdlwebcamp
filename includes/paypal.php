dac805cd25fc4fdd1236c1737fc20c662f6128d2
Carlos Talavera <carloseduardorivera12@gmail.com>
2019-01-27 13:26:26 -0600

    Update paypal.php

--- includes/paypal.php
+++ includes/paypal.php
@@ -2,11 +2,11 @@
 
 require 'paypal/autoload.php';
 
-define("URL_SITIO", "http://localhost/gdlwebcamp"); // Crear una constante para utilizar el dominio general del sitio
+define("URL_SITIO", ""); // Crear una constante para utilizar el dominio general del sitio, por ejemplo 'http://localhost/gdlwebcamp'
 
 $apiContext = new \PayPal\Rest\ApiContext(
   new \PayPal\Auth\OAuthTokenCredential(
-    'AQ_mdXqUq_MEDCCic9HTrsgVUkBwOK0vYNFrC-X4-5DxeewtEK3H92H-PQ8w4pVk9QF4QZiCkToclshv', // ClienteID
-    'EKkbazGZBwVVSqTgthMxfEiS6OTtuO0oZS3qE5qYt6v72TrPOcTYLBOuNVwmp9-bLypaw_bMeiggcB8c' // Secret
+    '', // ClienteID
+    '' // Secret
   )
 );
