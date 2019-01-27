828e415297edd537b02a9957c5959a9760d0d3f0
Carlos Talavera <carloseduardorivera12@gmail.com>
2019-01-27 14:17:21 -0600

    DB SQL file added

---
+++ gdlwebcamp.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-01-2019 a las 21:15:57
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gdlwebcamp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admins`
+--
+
+CREATE TABLE `admins` (
+  `id_admin` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `foto_perfil` text NOT NULL,
+  `fecha_registro` varchar(50) NOT NULL,
+  `ult_edicion` varchar(50) NOT NULL,
+  `nivel` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria_evento`
+--
+
+CREATE TABLE `categoria_evento` (
+  `id_categoria` tinyint(4) NOT NULL,
+  `cat_evento` varchar(20) NOT NULL,
+  `icono` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventos`
+--
+
+CREATE TABLE `eventos` (
+  `evento_id` tinyint(10) NOT NULL,
+  `nombre_evento` varchar(60) NOT NULL,
+  `fecha_evento` date NOT NULL,
+  `hora_evento` time NOT NULL,
+  `id_cat_evento` tinyint(10) NOT NULL,
+  `id_inv` tinyint(4) NOT NULL,
+  `clave` varchar(10) NOT NULL,
+  `ult_edicion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `invitados`
+--
+
+CREATE TABLE `invitados` (
+  `invitado_id` tinyint(4) NOT NULL,
+  `nombre_invitado` varchar(30) NOT NULL,
+  `apellido_invitado` varchar(30) NOT NULL,
+  `descripcion` text NOT NULL,
+  `url_imagen` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `regalos`
+--
+
+CREATE TABLE `regalos` (
+  `ID_regalo` int(11) NOT NULL,
+  `nombre_regalo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registrados`
+--
+
+CREATE TABLE `registrados` (
+  `ID_Registrado` bigint(20) UNSIGNED NOT NULL,
+  `nombre_registrado` varchar(50) NOT NULL,
+  `apellido_registrado` varchar(50) NOT NULL,
+  `email_registrado` varchar(100) NOT NULL,
+  `fecha_registro` datetime NOT NULL,
+  `pases_articulos` longtext NOT NULL,
+  `talleres_registrados` longtext NOT NULL,
+  `regalo` int(11) NOT NULL,
+  `total_pagado` varchar(50) NOT NULL,
+  `pagado` int(1) NOT NULL,
+  `ult_edicion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- Indices de la tabla `categoria_evento`
+--
+ALTER TABLE `categoria_evento`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`evento_id`),
+  ADD KEY `id_cat_evento` (`id_cat_evento`),
+  ADD KEY `id_inv` (`id_inv`);
+
+--
+-- Indices de la tabla `invitados`
+--
+ALTER TABLE `invitados`
+  ADD PRIMARY KEY (`invitado_id`);
+
+--
+-- Indices de la tabla `regalos`
+--
+ALTER TABLE `regalos`
+  ADD PRIMARY KEY (`ID_regalo`);
+
+--
+-- Indices de la tabla `registrados`
+--
+ALTER TABLE `registrados`
+  ADD PRIMARY KEY (`ID_Registrado`),
+  ADD KEY `regalo` (`regalo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria_evento`
+--
+ALTER TABLE `categoria_evento`
+  MODIFY `id_categoria` tinyint(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `evento_id` tinyint(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `invitados`
+--
+ALTER TABLE `invitados`
+  MODIFY `invitado_id` tinyint(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `regalos`
+--
+ALTER TABLE `regalos`
+  MODIFY `ID_regalo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `registrados`
+--
+ALTER TABLE `registrados`
+  MODIFY `ID_Registrado` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `eventos`
+--
+ALTER TABLE `eventos`
+  ADD CONSTRAINT `eventos_ibfk_1` FOREIGN KEY (`id_cat_evento`) REFERENCES `categoria_evento` (`id_categoria`),
+  ADD CONSTRAINT `eventos_ibfk_2` FOREIGN KEY (`id_inv`) REFERENCES `invitados` (`invitado_id`);
+
+--
+-- Filtros para la tabla `registrados`
+--
+ALTER TABLE `registrados`
+  ADD CONSTRAINT `registrados_ibfk_1` FOREIGN KEY (`regalo`) REFERENCES `regalos` (`ID_regalo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
